62edaccdbb30f60e6140054b7b739e9a46e16781
eidmuha <eidmohd89@gmail.com>
2019-09-19 16:08:57 +1000

    routers, logics

--- modals/burger.js
+++ modals/burger.js
@@ -1,8 +1,31 @@
+// Import the ORM to create functions that will interact with the database
 var orm = require("../config/orm.js");
 
-
 // calling the orm functions
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, val, cb) {
+        console.log("cols: "+cols)
+        console.log("val: "+val)
+      orm.insertOne("burgers", cols, val, function(res) {
+          console.log(cols)
+          console.log(val)
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+       
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
 
 
-
-// export the burger 
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
